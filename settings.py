5a0a3bc66c76196dd88705470f877f6cd9093f5c
mouradghaly <101068633+mouradghaly@users.noreply.github.com>
2024-07-28 17:51:13 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os as os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-5v7)nbd=l20c_b@3*jsku3aeuf_e39*mp=jyapnrrf-cpp-3ix
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['iftarramadan.onrender.com localhost']
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split(" ")
 
 
 # Application definition
